8f1b9c9eafc84fde428aefe4914b533486ed68a9
Jam52 <jamie.sajdak@gmail.com>
2020-09-02 12:52:05 -0400

    Fix: adding miniCssExtractPlugin.loader scss files, add image-webpack-loader to minifi image files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,45 +1,62 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/client/index.js',
+    mode: "production",
+    entry: "./src/client/index.js",
     devtool: false,
     module: {
         rules: [
             {
                 test: /\.html$/i,
-                loader: 'html-loader',
+                loader: "html-loader",
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: "babel-loader",
             },
             {
                 test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
             },
             {
-                test: /\.(png|jp(e*)g|svg)$/i,  
-                use: [{
-                    loader: 'url-loader',
-                    options: { 
-                        limit: 8000,
-                        name: 'images/[hash]-[name].[ext]'
-                    } 
-                }]
-            }
-        ]
+                test: /\.(png|jp(e*)g|svg)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 10 * 1024,
+                            name: "images/[hash]-[name].[ext]",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/i,
+                loader: "image-webpack-loader",
+                // Specify enforce: 'pre' to apply the loader
+                // before url-loader/svg-url-loader
+                // and not duplicate it in rules with them
+                enforce: "pre",
+            },
+        ],
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             minify: false,
-            filename: 'index.html',
-            template: './src/client/views/index.html'
+            filename: "index.html",
+            template: "./src/client/views/index.html",
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
@@ -48,12 +65,13 @@ module.exports = {
             verbose: true,
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
+            protectWebpackAssets: false,
         }),
-        new MiniCssExtractPlugin ({filename:'[name].styles.css'}),
+        new MiniCssExtractPlugin({ filename: "[name].styles.css" }),
+        new OptimizeCSSAssetsPlugin({}),
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-      }
-}
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
